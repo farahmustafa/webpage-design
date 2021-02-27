a7a9881109ea7b6dff34a6b9d21bdcee8f105c50
farahmustafa <farahmustafa998@gmail.com>
2021-02-27 21:48:46 +0200

    commit first

---
+++ style/javascript/script.js
@@ -0,0 +1,8 @@
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(31.963158,35.930359),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
